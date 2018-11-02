bfb55932c9ac12b172fb218137acf7e1afb1d681
Donkemezuo <raymonddonkemezuo.org>
2018-11-01 22:19:36 -0400

    Latest work

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 
-
+//
 func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
     
     switch opString {
@@ -26,69 +26,147 @@ func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
     }
 }
 
-print("Enter some numbers")
-while true  {
+print("RayElectronics Inc 💻! Please enter prefered type of calculator |Regular or High order?|")
+
+let userChoice = readLine()
+
+if var userChoiceOfFunction = userChoice {
+
+switch userChoice {
+case "Regular":
     
-    let userInput = readLine()
+    print(" Please enter your operation")
     
-    if let myUserInput = userInput {
+    while true  {
         
-        let componentUserInput = myUserInput.components(separatedBy: " ")
-        guard componentUserInput.count == 3 else {
-            print("Invalid input format. Input values with space")
-            continue
-        }
+        let userInput = readLine()
         
-        let firstInput = componentUserInput[0]
-        let operatoraInput = componentUserInput[1]
-        let secondInput = componentUserInput[2]
-        if let myFirstInput = Double(firstInput){
+        if let myUserInput = userInput {
             
-            if let mySecondInput = Double(secondInput){
-                
-        
-        if operatoraInput == "?" {
-            var computerRandomOperator = ""
-            let arrayOfOperators = ["+", "-", "*", "/"]
-            if let randomOperator = arrayOfOperators.randomElement(){
-                computerRandomOperator = randomOperator
-                let myNewClosure = mathStuffFactory(opString: computerRandomOperator)
-                let result = myNewClosure(myFirstInput,mySecondInput)
-                print(" The outcome of \(myFirstInput) ? \(mySecondInput) is = \(result)")
-                
+            let componentUserInput = myUserInput.components(separatedBy: " ")
+            guard componentUserInput.count == 3 else {
+                print("Invalid input format. Input values with space")
+                continue
             }
-            print("Now guess the random operator")
-           let userOperatorInput = readLine()
-            if let myUserOperatorInput = userOperatorInput {
-guard myUserOperatorInput.count == 1 && myUserOperatorInput == "" else {
-                    print("Please you can only input an operators like +, -, *, / ")
-                    continue
-                }
-
-if myUserOperatorInput == computerRandomOperator {
-                    print("You got it right")
-                } else {
-                    if myUserOperatorInput != computerRandomOperator {
-                    print("You input operator is wrong. The right operator is \(computerRandomOperator)")
+            
+            let firstInput = componentUserInput[0]
+            let operatoraInput = componentUserInput[1]
+            let secondInput = componentUserInput[2]
+            if let myFirstInput = Double(firstInput){
+                
+                if let mySecondInput = Double(secondInput){
+                    
+                    
+                    if operatoraInput == "?" {
+                        var computerRandomOperator = ""
+                        let arrayOfOperators = ["+", "-", "*", "/"]
+                        if let randomOperator = arrayOfOperators.randomElement(){
+                            computerRandomOperator = randomOperator
+                            let myNewClosure = mathStuffFactory(opString: computerRandomOperator)
+                            let result = myNewClosure(myFirstInput,mySecondInput)
+                            print("\(myFirstInput) ? \(mySecondInput) = \(result)")
+                            
+                        }
+                        print("Now guess the random operator")
+                        let userOperatorInput = readLine()
+                        if let myUserOperatorInput = userOperatorInput {
+                            guard arrayOfOperators.contains(myUserOperatorInput)  /*.count == 1 && myUserOperatorInput == ""*/ else {
+                                print("Please you can only input an operators like +, -, *, / ")
+                                continue
+                            }
+                            
+                            if myUserOperatorInput == computerRandomOperator {
+                                print("You got it right")
+                            } else {
+                                if myUserOperatorInput != computerRandomOperator {
+                                    print("You input operator is wrong. The right operator is \(computerRandomOperator)")
+                                }
+                            }
+                            continue
+                        }
                     }
+                    let closure = mathStuffFactory(opString: operatoraInput)
+                    let closureReturns = closure(myFirstInput, mySecondInput)
+                    print("\(myFirstInput) \(operatoraInput) \(mySecondInput) = \(closureReturns)")
+                    
                 }
+            }
+}
+}
+    
+    
+case: "High order"
 
-            continue
+print("we have high order functions like filter, map and reduce. Please enter your prefered choice")
+let userInputt = readLine()
+
+if let myUserChoiceOfOperation = userInputt {
+    let theComponents = myUserChoiceOfOperation.components(separatedBy: " ")
+    
+    var highOrderFunction = theComponents[0]
+    var unfilteredArray = theComponents[1]
+    var filterBy = theComponents[3]
+    var numFilteredBy = Int(theComponents[4])
+    let myUnwrappedFilteredBy = numFilteredBy ?? 0
+    
+    switch filterBy {
+    case "<":
+        
+        func filter(inputArray:[Int], filter: (Int) -> Bool) -> [Int] {
+            var filteredNumbers:[Int] = []
+            for myNumber in inputArray {
+                if myNumber < myUnwrappedFilteredBy {
+                    filteredNumbers.append(myNumber)
+                }
+            }
+            return filteredNumbers
+            
         }
         
+    case ">":
+        func filter(inputArray:[Int], filter: (Int) -> Bool) -> [Int] {
+            var filteredNumbers:[Int] = []
+            for myNumber in inputArray {
+                if myNumber > myUnwrappedFilteredBy {
+                    filteredNumbers.append(myNumber)
+                }
             }
-                
-    let closure = mathStuffFactory(opString: operatoraInput)
-                let closureReturns = closure(myFirstInput, mySecondInput)
-                print("The result of \(myFirstInput) \(operatoraInput) \(mySecondInput) is \(closureReturns)")
-                
+            return filteredNumbers
+            
         }
+    default:
+        print("invalid input")
     }
-//
-//            print("Check your input spaces")
+    }
+    }
+    
+    
+    
+    func map(inputArray:[Int]) -> [Int]{
+        var numbersInArray:[Int] = []
+        for number in inputArray {
+            numbersInArray += number * userInputtt
+            return numbersInArray
+        }
+        
+    }
+    
+    
+    func map(inputArray:[Int]) -> [Int]{
+        var numbersInArray:[Int] = []
+        for number in inputArray {
+            numbersInArray += number / userInputtt
+            return numbersInArray
+        }
+        
+
+    
+default:
+print("Return to menu")
     
 }
-
-
-
+  
 }
+
+
+
